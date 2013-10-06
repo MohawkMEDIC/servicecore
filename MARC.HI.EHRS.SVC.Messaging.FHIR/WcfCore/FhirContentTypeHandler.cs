e0febdccc30b396d9443006f2f180da7239636df
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2013-10-06 20:38:03 +0000

    Added missing content type handler
    git-svn-id: https://fisheye.marc-hi.ca/svn/servicecore/trunk@83 95063a73-f08f-3c4f-b139-79b229f59857

---
+++ MARC.HI.EHRS.SVC.Messaging.FHIR/WcfCore/FhirContentTypeHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Channels;
+
+namespace MARC.HI.EHRS.SVC.Messaging.FHIR.WcfCore
+{
+    public class FhirContentTypeHandler : WebContentTypeMapper
+    {
+
+        public override WebContentFormat GetMessageFormatForContentType(string contentType)
+        {
+
+            if (contentType.StartsWith("application/fhir+xml", StringComparison.OrdinalIgnoreCase))
+            {
+                return WebContentFormat.Xml;
+            }
+            else
+            {
+                return WebContentFormat.Default;
+            }
+
+        }
+
+    }
+
+}
