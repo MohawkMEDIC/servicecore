8e2e7a59f85c25cc9c0ca80758800b8c51041004
jf03cg <justin@fyfesoftware.ca>
2015-12-29 00:42:10 -0500

    Started migration to FHIR DSTU2

---
+++ MARC.HI.EHRS.SVC.Messaging.FHIR/Resources/Bundle.cs
@@ -0,0 +1,79 @@
+﻿using MARC.HI.EHRS.SVC.Messaging.FHIR.Attributes;
+using MARC.HI.EHRS.SVC.Messaging.FHIR.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MARC.HI.EHRS.SVC.Messaging.FHIR.Resources
+{
+    /// <summary>
+    /// Identifies the type of bundles 
+    /// </summary>
+    [XmlType("BundleType", Namespace = "http://hl7.org/fhir")]
+    public enum BundleType
+    {
+        [XmlEnum("document")]
+        Document,
+        [XmlEnum("message")]
+        Message,
+        [XmlEnum("transaction")]
+        Transaction,
+        [XmlEnum("transaction-response")]
+        TransactionResponse,
+        [XmlEnum("batch")]
+        Batch,
+        [XmlEnum("batch-response")]
+        BatchResponse,
+        [XmlEnum("history")]
+        HistoryList,
+        [XmlEnum("searchset")]
+        SearchResults,
+        [XmlEnum("collection")]
+        Collection
+    }
+
+    /// <summary>
+    /// Represents a bundle of resources. DSTU2 replacement for feeds
+    /// </summary>
+    [XmlType("Bundle", Namespace = "http://hl7.org/fhir")]
+    public class Bundle : ResourceBase
+    {
+
+        /// <summary>
+        /// Gets or sets the type of the bundle
+        /// </summary>
+        [XmlElement("type")]
+        [ElementProfile(Binding = typeof(BundleType), Comment = "The type of bundle", MaxOccurs = 1, MinOccurs = 1, MustSupport = true)]
+        [Description("Identifies the type of bundle")]
+        public PrimitiveCode<BundleType> Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number of search results
+        /// </summary>
+        [XmlElement("total")]
+        [Description("If search, the total number of search results")]
+        public FhirInt Total { get; set; }
+
+        /// <summary>
+        /// Gets or sets a series of links related to the bundle
+        /// </summary>
+        [XmlElement("link")]
+        [Description("Links related to this bundle")]
+        public List<BundleLink> Link { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of bundle entries
+        /// </summary>
+        [XmlElement("entry")]
+        [Description("Entry in the bundle")]
+        public List<BundleEntry> Entry { get; set; }
+
+        [XmlElement("signature")]
+        [Description("Digital signature for the bundle contents")]
+        public Signature Signature { get; set; }
+    }
+}
