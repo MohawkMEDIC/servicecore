f77b5fc0680742047ef8d03dee11a744b8f0274d
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-10-02 16:46:06 -0400

    automated code cleanup

--- MARC.HI.EHRS.SVC.Messaging.HAPI/HL7MessageHandler.cs
+++ MARC.HI.EHRS.SVC.Messaging.HAPI/HL7MessageHandler.cs
@@ -1,124 +1,123 @@
-﻿/**
+﻿using MARC.HI.EHRS.SVC.Core;
+using MARC.HI.EHRS.SVC.Core.Services;
+using MARC.HI.EHRS.SVC.Messaging.HAPI.Configuration;
+
+/**
  * Copyright 2012-2013 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 13-8-2012
  */
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using MARC.HI.EHRS.SVC.Core.Services;
-using System.Threading;
-using MARC.HI.EHRS.SVC.Messaging.HAPI.Configuration;
-using System.Configuration;
 using System.Diagnostics;
-using MARC.HI.EHRS.SVC.Core;
+using System.Threading;
 
 namespace MARC.HI.EHRS.SVC.Messaging.HAPI
 {
-    /// <summary>
-    /// Message handler service
-    /// </summary>
-    public class HL7MessageHandler : IMessageHandlerService
-    {
-        #region IMessageHandlerService Members
+	/// <summary>
+	/// Message handler service
+	/// </summary>
+	public class HL7MessageHandler : IMessageHandlerService
+	{
+		#region IMessageHandlerService Members
 
-        // Configuration 
-        private HL7ConfigurationSection m_configuration;
+		// Configuration
+		private HL7ConfigurationSection m_configuration;
 
-        // Threads that are listening for messages
-        private List<Thread> m_listenerThreads = new List<Thread>();
+		// Threads that are listening for messages
+		private List<Thread> m_listenerThreads = new List<Thread>();
 
-        /// <summary>
-        /// Load configuration
-        /// </summary>
-        public HL7MessageHandler()
-        {
-            this.m_configuration = ApplicationContext.Current.GetService<IConfigurationManager>().GetSection("marc.hi.ehrs.svc.messaging.hapi") as HL7ConfigurationSection;
-        }
+		/// <summary>
+		/// Load configuration
+		/// </summary>
+		public HL7MessageHandler()
+		{
+			this.m_configuration = ApplicationContext.Current.GetService<IConfigurationManager>().GetSection("marc.hi.ehrs.svc.messaging.hapi") as HL7ConfigurationSection;
+		}
 
-        /// <summary>
-        /// Start the v2 message handler
-        /// </summary>
-        public bool Start()
-        {
-            this.Starting?.Invoke(this, EventArgs.Empty);
+		/// <summary>
+		/// Start the v2 message handler
+		/// </summary>
+		public bool Start()
+		{
+			this.Starting?.Invoke(this, EventArgs.Empty);
 
-            foreach (var sd in this.m_configuration.Services)
-            {
+			foreach (var sd in this.m_configuration.Services)
+			{
+				var sh = new ServiceHandler(sd);
+				Thread thdSh = new Thread(sh.Run);
+				thdSh.IsBackground = true;
+				this.m_listenerThreads.Add(thdSh);
+				Trace.TraceInformation("Starting HL7 Service '{0}'...", sd.Name);
+				thdSh.Start();
+			}
 
-                var sh = new ServiceHandler(sd);
-                Thread thdSh = new Thread(sh.Run);
-                thdSh.IsBackground = true;
-                this.m_listenerThreads.Add(thdSh);
-                Trace.TraceInformation("Starting HL7 Service '{0}'...", sd.Name);
-                thdSh.Start();
-            }
+			this.Started?.Invoke(this, EventArgs.Empty);
 
-            this.Started?.Invoke(this, EventArgs.Empty);
+			return true;
+		}
 
-            return true;
-        }
+		/// <summary>
+		/// Stop the v2 message handler
+		/// </summary>
+		public bool Stop()
+		{
+			this.Stopping?.Invoke(this, EventArgs.Empty);
+			foreach (var thd in this.m_listenerThreads)
+				if (thd.IsAlive)
+					thd.Abort();
+			this.Stopped?.Invoke(this, EventArgs.Empty);
+			return true;
+		}
 
-        /// <summary>
-        /// Stop the v2 message handler
-        /// </summary>
-        public bool Stop()
-        {
-            this.Stopping?.Invoke(this, EventArgs.Empty);
-            foreach (var thd in this.m_listenerThreads)
-                if (thd.IsAlive)
-                    thd.Abort();
-            this.Stopped?.Invoke(this, EventArgs.Empty);
-            return true;
-        }
+		#endregion IMessageHandlerService Members
 
-        #endregion
+		// Host context
+		private IServiceProvider m_context;
 
-        // Host context
-        private IServiceProvider m_context;
+		/// <summary>
+		/// Fired when the service has stopped
+		/// </summary>
+		public event EventHandler Started;
 
-        /// <summary>
-        /// Fired when the service is starting
-        /// </summary>
-        public event EventHandler Starting;
-        /// <summary>
-        /// Fired when the service is stopping
-        /// </summary>
-        public event EventHandler Stopping;
-        /// <summary>
-        /// Fired when the service has stopped
-        /// </summary>
-        public event EventHandler Started;
-        /// <summary>
-        /// Fired when the service has stopped
-        /// </summary>
-        public event EventHandler Stopped;
+		/// <summary>
+		/// Fired when the service is starting
+		/// </summary>
+		public event EventHandler Starting;
 
-        /// <summary>
-        /// Returns true with the service is running
-        /// </summary>
-        public bool IsRunning
-        {
-            get
-            {
-                return this.m_listenerThreads?.Count > 0;
-            }
-        }
+		/// <summary>
+		/// Fired when the service has stopped
+		/// </summary>
+		public event EventHandler Stopped;
 
-    }
+		/// <summary>
+		/// Fired when the service is stopping
+		/// </summary>
+		public event EventHandler Stopping;
+
+		/// <summary>
+		/// Returns true with the service is running
+		/// </summary>
+		public bool IsRunning
+		{
+			get
+			{
+				return this.m_listenerThreads?.Count > 0;
+			}
+		}
+	}
 }
--- MARC.HI.EHRS.SVC.Messaging.HAPI/TransportProtocol/TransportUtil.cs
+++ MARC.HI.EHRS.SVC.Messaging.HAPI/TransportProtocol/TransportUtil.cs
@@ -1,83 +1,76 @@
 ﻿/**
  * Copyright 2012-2013 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: fyfej
  * Date: 13-8-2012
  */
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace MARC.HI.EHRS.SVC.Messaging.HAPI.TransportProtocol
 {
-    /// <summary>
-    /// Transport utilities
-    /// </summary>
-    internal static class TransportUtil
-    {
+	/// <summary>
+	/// Transport utilities
+	/// </summary>
+	internal static class TransportUtil
+	{
+		/// <summary>
+		/// Transport protocols
+		/// </summary>
+		private static Dictionary<String, Type> s_prots = new Dictionary<string, Type>();
 
-        /// <summary>
-        /// Transport protocols
-        /// </summary>
-        private static Dictionary<String, Type> s_prots = new Dictionary<string, Type>();
+		/// <summary>
+		/// Static ctor, construct protocol types
+		/// </summary>
+		static TransportUtil()
+		{
+			// Get all assemblies which have a transport protocol
+			var asm = typeof(TransportUtil).Assembly;
+			try
+			{
+				foreach (var typ in Array.FindAll(asm.GetTypes(), t => t.GetInterface(typeof(ITransportProtocol).FullName) != null))
+				{
+					ConstructorInfo ci = typ.GetConstructor(Type.EmptyTypes);
+					if (ci == null)
+						throw new InvalidOperationException(String.Format("Cannot find parameterless constructor for type '{0}'", typ.AssemblyQualifiedName));
+					ITransportProtocol tp = ci.Invoke(null) as ITransportProtocol;
+					s_prots.Add(tp.ProtocolName, typ);
+				}
+			}
+			catch (Exception e)
+			{
+				Trace.TraceError(e.ToString());
+			}
+		}
 
-        /// <summary>
-        /// Static ctor, construct protocol types
-        /// </summary>
-        static TransportUtil()
-        {
+		/// <summary>
+		/// Create transport for the specified protocoltype
+		/// </summary>
+		internal static ITransportProtocol CreateTransport(string protocolType)
+		{
+			Type pType = null;
+			if (!s_prots.TryGetValue(protocolType, out pType))
+				throw new InvalidOperationException(String.Format("Cannot find protocol handler for '{0}'", protocolType));
 
-            // Get all assemblies which have a transport protocol
-            var asm = typeof(TransportUtil).Assembly;
-            try
-            {
-                foreach (var typ in Array.FindAll(asm.GetTypes(), t => t.GetInterface(typeof(ITransportProtocol).FullName) != null))
-                {
-                    ConstructorInfo ci = typ.GetConstructor(Type.EmptyTypes);
-                    if (ci == null)
-                        throw new InvalidOperationException(String.Format("Cannot find parameterless constructor for type '{0}'", typ.AssemblyQualifiedName));
-                    ITransportProtocol tp = ci.Invoke(null) as ITransportProtocol;
-                    s_prots.Add(tp.ProtocolName, typ);
-                }
-            }
-            catch (Exception e)
-            {
-                Trace.TraceError(e.ToString());
-            }
-        }
-
-
-        /// <summary>
-        /// Create transport for the specified protocoltype
-        /// </summary>
-        internal static ITransportProtocol CreateTransport(string protocolType)
-        {
-            Type pType = null;
-            if (!s_prots.TryGetValue(protocolType, out pType))
-                throw new InvalidOperationException(String.Format("Cannot find protocol handler for '{0}'", protocolType));
-
-            ConstructorInfo ci = pType.GetConstructor(Type.EmptyTypes);
-            if (ci == null)
-                throw new InvalidOperationException(String.Format("Cannot find parameterless constructor for type '{0}'", pType.AssemblyQualifiedName));
-            return ci.Invoke(null) as ITransportProtocol;
-            
-        }
-
-    }
+			ConstructorInfo ci = pType.GetConstructor(Type.EmptyTypes);
+			if (ci == null)
+				throw new InvalidOperationException(String.Format("Cannot find parameterless constructor for type '{0}'", pType.AssemblyQualifiedName));
+			return ci.Invoke(null) as ITransportProtocol;
+		}
+	}
 }
