d9252545ec9c0c4dbeeb16add63982ca4fa922f8
fyfej <fyfej@FYFEJ-PC>
2018-01-08 10:30:08 -0500

    Added trace source attribute which allows classes to self-declare what trace sources they expose. Added configuration to FHIR to allow the results to have a configured URI rather than absolute URI.

---
+++ MARC.HI.EHRS.SVC.Core/Attributes/TraceSourceAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARC.HI.EHRS.SVC.Core.Attributes
+{
+    /// <summary>
+    /// Allows a class to describe the trace source that it uses
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TraceSourceAttribute : Attribute
+    {
+
+        /// <summary>
+        /// Constructor which initializes the trace source name attribute
+        /// </summary>
+        public TraceSourceAttribute(String traceSourceName)
+        {
+            this.TraceSourceName = traceSourceName;
+        }
+
+        /// <summary>
+        /// Gets the trace source name
+        /// </summary>
+        public String TraceSourceName { get; private set; }
+    }
+}
