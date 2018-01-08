da2882c11916366bcd52b714484eb53b5f7dd65c
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-01-09 01:42:27 +0630

    Added encounter classes to FHIR interface

---
+++ MARC.HI.EHRS.SVC.Messaging.FHIR/Backbone/EncounterLocation.cs
@@ -0,0 +1,36 @@
+﻿using MARC.HI.EHRS.SVC.Messaging.FHIR.Attributes;
+using MARC.HI.EHRS.SVC.Messaging.FHIR.DataTypes;
+using MARC.HI.EHRS.SVC.Messaging.FHIR.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MARC.HI.EHRS.SVC.Messaging.FHIR.Backbone
+{
+    /// <summary>
+    /// A backbone element which allows expression of locations in an encounter
+    /// </summary>
+    [XmlType("EncounterLocation", Namespace = "http://hl7.org/fhir")]
+    public class EncounterLocation : BackboneElement
+    {
+
+        /// <summary>
+        /// Gets or sets the location
+        /// </summary>
+        [XmlElement("location")]
+        [FhirElement(MinOccurs = 1)]
+        [Description("The location involved in the encounter")]
+        public Reference<Location> Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the period that this location was involved.
+        /// </summary>
+        [XmlElement("period")]
+        [Description("The period of involvement of this location")]
+        public FhirPeriod Period { get; set; }
+    }
+}
