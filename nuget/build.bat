db2fbb90da77ecb262b79b9d2b99d5460ce660a6
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-06-21 10:35:44 -0400

    Update to build script

--- nuget/build.bat
+++ nuget/build.bat
@@ -2,15 +2,26 @@
 
 cmd /c del release\*.nupkg
 
+
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-	echo will use VS 2017 build tools
-	"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:clean /m
-	"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:rebuild /p:configuration=release /m
-) else ( echo Unable to locate VS 2017 build tools, will use default build tools )
+        echo will use VS 2017 build tools
+        set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+) else (
+	if exist "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
+        echo will use VS 2017 build tools
+        set msbuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+	) else ( echo Unable to locate VS 2017 build tools, will use default build tools )
+)
+
+if defined msbuild (
+
+%msbuild% ..\ServiceCore.sln /t:clean /m
+%msbuild% ..\ServiceCore.sln /t:rebuild /p:configuration=release /m
 
 for /D %%D in (..\MARC.HI.EHRS.*) do (	
 	echo found directory %%D
 	
 	echo will create nupkg file
 	nuget pack %%D -IncludeReferencedProjects -OutputDirectory ..\nuget\release -prop Configuration=Release
+)
 )
