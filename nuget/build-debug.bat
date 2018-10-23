cfd3ba0d6f171e8b6dddc0b4c6b15775cd059d32
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-10-23 12:20:50 +0630

    Updated nuget build

--- nuget/build-debug.bat
+++ nuget/build-debug.bat
@@ -4,11 +4,11 @@ cmd /c del release\*.nupkg
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
 	echo will use VS 2017 build tools
-	"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:clean /t:rebuild /p:configuration=Debug /m
+	"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:clean /t:restore /t:rebuild /p:configuration=Debug /m
 ) else ( 
 	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
 		echo will use VS 2017 build tools
-		"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:clean /t:rebuild /p:configuration=Debug /m
+		"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\ServiceCore.sln /t:clean /t:restore /t:rebuild /p:configuration=Debug /m
 	)
 	else (
 		echo Unable to locate VS 2017 build tools, will use default build tools 
